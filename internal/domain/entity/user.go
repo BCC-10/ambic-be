70b8a8d84da62923a01a297f8fe8fa9908b5ac3c
atha3417 <atha.3417@gmail.com>
2025-03-01 13:38:04 +0700

    chore: change column type of user entity

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -5,6 +5,13 @@ import (
 	"time"
 )
 
+type Gender string
+
+const (
+	Male   Gender = "male"
+	Female Gender = "female"
+)
+
 type User struct {
 	ID         uuid.UUID `gorm:"type:varchar(36);primaryKey"`
 	Name       string    `gorm:"type:varchar(255);not null"`
@@ -13,7 +20,7 @@ type User struct {
 	Phone      string    `gorm:"type:varchar(15);uniqueIndex;default:null"`
 	Address    string    `gorm:"type:text;default:null"`
 	BornDate   time.Time `gorm:"type:date;default:null"`
-	Gender     string    `gorm:"type:varchar(10);default:null"`
+	Gender     Gender    `gorm:"type:ENUM('male','female');default:null"`
 	Password   string    `gorm:"type:varchar(255)"`
 	IsVerified bool      `gorm:"type:boolean;default:false"`
 	PhotoURL   string    `gorm:"type:varchar(255);default:null"`
