b325174e5a7a8541a7f834d7c3cb126d58bf6223
atha3417 <atha.3417@gmail.com>
2025-03-04 00:06:42 +0700

    chore: set register partner request only can receive app/json content

--- internal/domain/dto/partner.go
+++ internal/domain/dto/partner.go
@@ -1,10 +1,10 @@
 package dto
 
 type RegisterPartnerRequest struct {
-	Name      string  `form:"name" json:"name" validate:"required"`
-	Type      string  `form:"type" json:"type" validate:"required"`
-	Address   string  `form:"address" json:"address" validate:"required"`
-	City      string  `form:"city" json:"city" validate:"required"`
-	Longitude float64 `form:"longitude" json:"longitude" validate:"required,longitude"`
-	Latitude  float64 `form:"latitude" json:"latitude" validate:"required,latitude"`
+	Name      string  `form:"name" validate:"required"`
+	Type      string  `form:"type" validate:"required"`
+	Address   string  `form:"address" validate:"required"`
+	City      string  `form:"city" validate:"required"`
+	Longitude float64 `form:"longitude" validate:"required,longitude"`
+	Latitude  float64 `form:"latitude" validate:"required,latitude"`
 }
