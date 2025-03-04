ed2d9fc9118789cf0ddc2cbebae6936997e4cafa
atha3417 <atha.3417@gmail.com>
2025-03-04 15:19:10 +0700

    chore: append business type id to partner entity

--- internal/domain/entity/partner.go
+++ internal/domain/entity/partner.go
@@ -7,18 +7,20 @@ import (
 )
 
 type Partner struct {
-	ID         uuid.UUID `gorm:"type:varchar(36);primary_key"`
-	Products   []Product
-	UserID     uuid.UUID `gorm:"type:varchar(36);not null;"`
-	Name       string    `gorm:"type:varchar(255);not null"`
-	Type       string    `gorm:"type:varchar(255);not null"`
-	Address    string    `gorm:"type:varchar(255);not null"`
-	City       string    `gorm:"type:varchar(255);not null"`
-	Longitude  float64   `gorm:"type:float;not null"`
-	Latitude   float64   `gorm:"type:float;not null"`
-	IsVerified bool      `gorm:"type:boolean;default:false"`
-	CreatedAt  time.Time `gorm:"type:timestamp;autoCreateTime"`
-	UpdatedAt  time.Time `gorm:"type:timestamp;autoUpdateTime"`
+	ID             uuid.UUID `gorm:"type:varchar(36);primary_key"`
+	UserID         uuid.UUID `gorm:"type:varchar(36);not null;"`
+	BusinessTypeID uint
+	Products       []Product
+	Name           string    `gorm:"type:varchar(255);not null"`
+	Type           string    `gorm:"type:varchar(255);not null"`
+	Address        string    `gorm:"type:varchar(255);not null"`
+	City           string    `gorm:"type:varchar(255);not null"`
+	Longitude      float64   `gorm:"type:float;not null"`
+	Latitude       float64   `gorm:"type:float;not null"`
+	Instagram      string    `gorm:"type:varchar(255);not null"`
+	IsVerified     bool      `gorm:"type:boolean;default:false"`
+	CreatedAt      time.Time `gorm:"type:timestamp;autoCreateTime"`
+	UpdatedAt      time.Time `gorm:"type:timestamp;autoUpdateTime"`
 }
 
 func (p *Partner) BeforeCreate(tx *gorm.DB) (err error) {
