516cd826645a4fc72b2eec68145ab5db12656300
atha3417 <atha.3417@gmail.com>
2025-03-04 15:16:51 +0700

    chore: delete social media entity

--- internal/domain/entity/social_media.go
+++
@@ -1,7 +0,0 @@
-package entity
-
-type SocialMedia struct {
-	ID       uint   `gorm:"primary_key"`
-	Media    string `gorm:"type:varchar(255);not null"`
-	Username string `gorm:"type:varchar(255);not null"`
-}
