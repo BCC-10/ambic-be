a5bc9bb7f8a26b67bd95f7941c5a0083cff95783
atha3417 <atha.3417@gmail.com>
2025-03-05 13:43:51 +0700

    feat: dto parser

--- internal/domain/entity/rating.go
+++ internal/domain/entity/rating.go
@@ -1,6 +1,9 @@
 package entity
 
-import "github.com/google/uuid"
+import (
+	"ambic/internal/domain/dto"
+	"github.com/google/uuid"
+)
 
 type Rating struct {
 	ID        uint      `gorm:"primaryKey"`
@@ -9,4 +12,15 @@ type Rating struct {
 	Star      int       `gorm:"type:int(8);not null"`
 	Feedback  string    `gorm:"type:varchar(1000);not null"`
 	PhotoURL  string    `gorm:"type:varchar(255)"`
+}
+
+func (r *Rating) ParseDTOGet() dto.GetRatingResponse {
+	return dto.GetRatingResponse{
+		ID:        r.ID,
+		ProductID: r.ProductID.String(),
+		UserID:    r.UserID.String(),
+		Star:      r.Star,
+		Feedback:  r.Feedback,
+		Photo:     r.PhotoURL,
+	}
 }
