fed5ec981c65e761212aac876c617796c5d770f8
atha3417 <atha.3417@gmail.com>
2025-03-05 12:19:23 +0700

    chore: remove ensure partner middleware when hit verification endpoint

--- internal/app/partner/interface/rest/partner.go
+++ internal/app/partner/interface/rest/partner.go
@@ -25,9 +25,9 @@ func NewPartnerHandler(routerGroup fiber.Router, partnerUsecase usecase.PartnerU
 	}
 
 	routerGroup = routerGroup.Group("/partners")
-	routerGroup.Get("/:id/products", PartnerHandler.GetProducts)
+	routerGroup.Get("/:id/products", m.Authentication, m.EnsurePartner, PartnerHandler.GetProducts)
 	routerGroup.Post("/register", m.Authentication, m.EnsureNotPartner, PartnerHandler.RegisterPartner)
-	routerGroup.Post("/verify", m.Authentication, m.EnsurePartner, PartnerHandler.VerifyPartner)
+	routerGroup.Post("/verify", m.Authentication, PartnerHandler.VerifyPartner)
 }
 
 func (h *PartnerHandler) RegisterPartner(ctx *fiber.Ctx) error {
