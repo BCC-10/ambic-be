953371a12b19bd1310db437215ea60f19ac1d9fe
atha3417 <atha.3417@gmail.com>
2025-03-05 07:45:22 +0700

    chore: add body limit

--- internal/infra/fiber/fiber.go
+++ internal/infra/fiber/fiber.go
@@ -1,15 +1,17 @@
 package fiber
 
 import (
+	"ambic/internal/domain/env"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"time"
 )
 
-func New() *fiber.App {
+func New(env *env.Env) *fiber.App {
 	app := fiber.New(fiber.Config{
 		IdleTimeout: 5 * time.Second,
+		BodyLimit:   int(env.MaxUploadSize * 1024 * 1024),
 	})
 
 	app.Use(logger.New())
