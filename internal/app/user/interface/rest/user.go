5c826ffe9ebe3d92bfa4b240fb0ddba2f6229abd
atha3417 <atha.3417@gmail.com>
2025-03-05 10:40:05 +0700

    chore: use form parser

--- internal/app/user/interface/rest/user.go
+++ internal/app/user/interface/rest/user.go
@@ -3,6 +3,7 @@ package rest
 import (
 	"ambic/internal/app/user/usecase"
 	"ambic/internal/domain/dto"
+	"ambic/internal/infra/helper"
 	res "ambic/internal/infra/response"
 	"ambic/internal/middleware"
 	"github.com/go-playground/validator/v10"
@@ -27,27 +28,12 @@ func NewUserHandler(routerGroup fiber.Router, userUsecase usecase.UserUsecaseItf
 
 func (h UserHandler) UpdateUser(ctx *fiber.Ctx) error {
 	req := new(dto.UpdateUserRequest)
-	if ctx.Get("Content-Type") == "application/json" {
-		if err := ctx.BodyParser(req); err != nil {
-			return res.BadRequest(ctx)
-		}
-	} else {
-		req = &dto.UpdateUserRequest{
-			Name:     ctx.FormValue("name"),
-			Phone:    ctx.FormValue("phone"),
-			Address:  ctx.FormValue("address"),
-			BornDate: ctx.FormValue("born_date"),
-			Gender:   ctx.FormValue("gender"),
-		}
+	if err := helper.FormParser(ctx, req); err != nil {
+		return res.BadRequest(ctx)
 	}
 
 	if err := h.Validator.Struct(req); err != nil {
 		return res.ErrValidationError(ctx, err)
-	}
-
-	file, err := ctx.FormFile("photo")
-	if err == nil {
-		req.Photo = file
 	}
 
 	userId := ctx.Locals("userId").(uuid.UUID)
