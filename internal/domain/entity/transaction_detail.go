a3cc6fe9a98d5b5a5eae4411abeaf7fbfb52196f
atha3417 <atha.3417@gmail.com>
2025-03-06 17:02:34 +0700

    feat: transaction detail id

---
+++ internal/domain/entity/transaction_detail.go
@@ -0,0 +1,19 @@
+package entity
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type TransactionDetail struct {
+	ID            uuid.UUID `gorm:"type:char(36);primaryKey"`
+	TransactionID uuid.UUID `gorm:"type:char(36);not null"`
+	ProductID     uuid.UUID `gorm:"type:char(36);not null"`
+	Qty           int       `gorm:"type:int;not null"`
+}
+
+func (d *TransactionDetail) BeforeCreate(tx *gorm.DB) (err error) {
+	id, _ := uuid.NewUUID()
+	d.ID = id
+	return
+}
