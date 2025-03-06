c388f3ca5462c41797c6b3affa0da7c1264b4de8
atha3417 <atha.3417@gmail.com>
2025-03-06 22:25:56 +0700

    chore: add GetPaymentResponse DTO for payment retrieval

--- internal/domain/dto/payment.go
+++ internal/domain/dto/payment.go
@@ -1,5 +1,7 @@
 package dto
 
+import "github.com/google/uuid"
+
 type NotificationPayment struct {
 	OrderID           string `json:"order_id" validate:"required"`
 	ReferenceID       string `json:"reference_id" validate:"required"`
@@ -9,4 +11,17 @@ type NotificationPayment struct {
 	FraudStatus       string `json:"fraud_status" validate:"required"`
 	TransactionTime   string `json:"transaction_time" validate:"required"`
 	SettlementTime    string `json:"settlement_time" validate:"required"`
+}
+
+type GetPaymentResponse struct {
+	ID                uuid.UUID `json:"id"`
+	TransactionID     uuid.UUID `json:"transaction_id"`
+	OrderID           string    `json:"order_id"`
+	ReferenceID       string    `json:"reference_id"`
+	TransactionStatus string    `json:"transaction_status"`
+	StatusMessage     string    `json:"status_message"`
+	PaymentType       string    `json:"payment_type"`
+	FraudStatus       string    `json:"fraud_status"`
+	TransactionTime   string    `json:"transaction_time"`
+	SettlementTime    string    `json:"settlement_time"`
 }
