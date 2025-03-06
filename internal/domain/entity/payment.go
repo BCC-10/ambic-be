1d228713c48864f5dd808bb309b77cda2737443c
atha3417 <atha.3417@gmail.com>
2025-03-06 22:26:16 +0700

    chore: add CreatedAt and UpdatedAt fields to Payment entity and implement ParseDTOGet method

--- internal/domain/entity/payment.go
+++ internal/domain/entity/payment.go
@@ -1,6 +1,7 @@
 package entity
 
 import (
+	"ambic/internal/domain/dto"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"time"
@@ -16,10 +17,26 @@ type Payment struct {
 	FraudStatus       string    `gorm:"type:varchar(255);not null"`
 	TransactionTime   time.Time `gorm:"type:timestamp"`
 	SettlementTime    time.Time `gorm:"type:timestamp"`
+	CreatedAt         time.Time `gorm:"type:timestamp;autoCreateTime"`
+	UpdatedAt         time.Time `gorm:"type:timestamp;autoUpdateTime"`
 }
 
 func (u *Payment) BeforeCreate(tx *gorm.DB) (err error) {
 	id, _ := uuid.NewUUID()
 	u.ID = id
 	return
+}
+
+func (u *Payment) ParseDTOGet() dto.GetPaymentResponse {
+	return dto.GetPaymentResponse{
+		ID:                u.ID,
+		TransactionID:     u.TransactionID,
+		ReferenceID:       u.ReferenceID,
+		TransactionStatus: u.TransactionStatus,
+		StatusMessage:     u.StatusMessage,
+		PaymentType:       u.PaymentType,
+		FraudStatus:       u.FraudStatus,
+		TransactionTime:   u.TransactionTime.String(),
+		SettlementTime:    u.SettlementTime.String(),
+	}
 }
