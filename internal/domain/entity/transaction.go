fce9e598799cdc62ec7231bcb0c14cb9843a52cf
atha3417 <atha.3417@gmail.com>
2025-03-06 22:26:33 +0700

    chore: update Transaction entity to include Payment relationship and implement ParseDTOGet method

--- internal/domain/entity/transaction.go
+++ internal/domain/entity/transaction.go
@@ -1,6 +1,7 @@
 package entity
 
 import (
+	"ambic/internal/domain/dto"
 	"github.com/google/uuid"
 	"time"
 )
@@ -17,7 +18,7 @@ const (
 type Transaction struct {
 	ID                 uuid.UUID `gorm:"type:char(36);primaryKey"`
 	UserID             uuid.UUID `gorm:"type:char(36);not null"`
-	PaymentID          uuid.UUID `gorm:"type:char(36)"`
+	Payment            Payment
 	TransactionDetails []TransactionDetail
 	Invoice            string    `gorm:"type:varchar(255);not null;uniqueIndex"`
 	Total              float32   `gorm:"type:float(24);not null"`
@@ -25,4 +26,16 @@ type Transaction struct {
 	Note               string    `gorm:"type:text"`
 	CreatedAt          time.Time `gorm:"type:timestamp;autoCreateTime"`
 	UpdatedAt          time.Time `gorm:"type:timestamp;autoUpdateTime"`
+}
+
+func (t *Transaction) ParseDTOGet() dto.GetTransactionResponse {
+	return dto.GetTransactionResponse{
+		ID:        t.ID,
+		Payment:   t.Payment.ParseDTOGet(),
+		Invoice:   t.Invoice,
+		Total:     t.Total,
+		Status:    t.Status,
+		Note:      t.Note,
+		UpdatedAt: t.UpdatedAt,
+	}
 }
