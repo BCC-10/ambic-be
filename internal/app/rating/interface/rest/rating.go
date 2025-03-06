435e7d2870936b3406435c0f87a52eaceb39654f
atha3417 <atha.3417@gmail.com>
2025-03-06 12:20:35 +0700

    feat: create, update, delete feature

--- internal/app/rating/interface/rest/rating.go
+++ internal/app/rating/interface/rest/rating.go
@@ -1,7 +1,87 @@
 package rest
 
-type RatingHandler struct{}
+import (
+	RatingUsecase "ambic/internal/app/rating/usecase"
+	"ambic/internal/domain/dto"
+	"ambic/internal/infra/helper"
+	res "ambic/internal/infra/response"
+	"ambic/internal/middleware"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
 
-func NewRatingHandler() {
-	RatingHandler := RatingHandler{}
+type RatingHandler struct {
+	RatingUsecase RatingUsecase.RatingUsecaseItf
+	validator     *validator.Validate
+	helper        helper.HelperIf
+}
+
+func NewRatingHandler(routerGroup fiber.Router, ratingUsecase RatingUsecase.RatingUsecaseItf, validator *validator.Validate, m middleware.MiddlewareIf, helper helper.HelperIf) {
+	RatingHandler := RatingHandler{
+		RatingUsecase: ratingUsecase,
+		validator:     validator,
+		helper:        helper,
+	}
+
+	routerGroup = routerGroup.Group("/ratings")
+	routerGroup.Post("/", m.Authentication, RatingHandler.Create)
+	routerGroup.Patch("/:id/update", m.Authentication, RatingHandler.Update)
+	routerGroup.Delete("/:id", m.Authentication, RatingHandler.Delete)
+}
+
+func (h *RatingHandler) Create(ctx *fiber.Ctx) error {
+	req := new(dto.CreateRatingRequest)
+	if err := h.helper.FormParser(ctx, req); err != nil {
+		return res.BadRequest(ctx)
+	}
+
+	if err := h.validator.Struct(req); err != nil {
+		return res.ValidationError(ctx, nil, err)
+	}
+
+	userId := ctx.Locals("userId").(uuid.UUID)
+	if err := h.RatingUsecase.Create(userId, *req); err != nil {
+		return res.Error(ctx, err)
+	}
+
+	return res.SuccessResponse(ctx, res.RatingCreateSuccess, nil)
+}
+
+func (h *RatingHandler) Update(ctx *fiber.Ctx) error {
+	req := new(dto.UpdateRatingRequest)
+	if err := h.helper.FormParser(ctx, req); err != nil {
+		return res.BadRequest(ctx)
+	}
+
+	if err := h.validator.Struct(req); err != nil {
+		return res.ValidationError(ctx, nil, err)
+	}
+
+	userId := ctx.Locals("userId").(uuid.UUID)
+	ratingId, err := uuid.Parse(ctx.Params("id"))
+	if err != nil {
+		return res.BadRequest(ctx)
+	}
+
+	if err := h.RatingUsecase.Update(userId, ratingId, *req); err != nil {
+		return res.Error(ctx, err)
+	}
+
+	return res.SuccessResponse(ctx, res.RatingUpdateSuccess, nil)
+}
+
+func (h *RatingHandler) Delete(ctx *fiber.Ctx) error {
+	userId := ctx.Locals("userId").(uuid.UUID)
+
+	ratingId, err := uuid.Parse(ctx.Params("id"))
+	if err != nil {
+		return res.BadRequest(ctx)
+	}
+
+	if err := h.RatingUsecase.Delete(userId, ratingId); err != nil {
+		return res.Error(ctx, err)
+	}
+
+	return res.SuccessResponse(ctx, res.RatingDeleteSuccess, nil)
 }
--- internal/domain/dto/rating.go
+++ internal/domain/dto/rating.go
@@ -6,32 +6,29 @@ import (
 )
 
 type GetRatingResponse struct {
-	ID        uint   `json:"id"`
-	ProductID string `json:"product_id"`
-	UserID    string `json:"user_id"`
-	Star      int    `json:"star"`
-	Feedback  string `json:"feedback"`
-	Photo     string `json:"photo"`
+	ID        uuid.UUID `json:"id"`
+	ProductID string    `json:"product_id"`
+	UserID    string    `json:"user_id"`
+	Star      int       `json:"star"`
+	Feedback  string    `json:"feedback"`
+	Photo     string    `json:"photo"`
 }
 
 type CreateRatingRequest struct {
 	ProductID string                `form:"product_id" validate:"required,uuid"`
-	UserID    string                `form:"user_id" validate:"required,uuid"`
 	Star      int                   `form:"star" validate:"required,min=1,max=5"`
 	Feedback  string                `form:"feedback"`
 	Photo     *multipart.FileHeader `form:"photo"`
 }
 
 type UpdateRatingRequest struct {
-	ProductID string                `form:"product_id" validate:"required,uuid"`
-	UserID    string                `form:"user_id" validate:"required,uuid"`
-	Star      int                   `form:"star" validate:"required,min=1,max=5"`
-	Feedback  string                `form:"feedback"`
-	Photo     *multipart.FileHeader `form:"photo"`
+	Star     int                   `form:"star" validate:"required,min=1,max=5"`
+	Feedback string                `form:"feedback"`
+	Photo    *multipart.FileHeader `form:"photo"`
 }
 
 type RatingParam struct {
-	ID        uint
+	ID        uuid.UUID
 	ProductID uuid.UUID
 	UserID    uuid.UUID
 }
