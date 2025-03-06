6dd7405188863dedf2cebfee33031db9f38766d2
atha3417 <atha.3417@gmail.com>
2025-03-06 17:04:29 +0700

    chore: add relation to transaction

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -15,21 +15,22 @@ const (
 )
 
 type User struct {
-	ID         uuid.UUID `gorm:"type:varchar(36);primaryKey"`
-	Partner    Partner
-	Ratings    []Rating
-	Name       string    `gorm:"type:varchar(255)"`
-	Username   string    `gorm:"type:varchar(255);unique;not null"`
-	Email      string    `gorm:"type:varchar(255);unique;not null"`
-	Phone      string    `gorm:"type:varchar(15);uniqueIndex;default:null"`
-	Address    string    `gorm:"type:text;default:null"`
-	BornDate   time.Time `gorm:"type:date;default:null"`
-	Gender     *Gender   `gorm:"type:ENUM('male','female');default:null"`
-	Password   string    `gorm:"type:varchar(255)"`
-	IsVerified bool      `gorm:"type:boolean;default:false"`
-	PhotoURL   string    `gorm:"type:varchar(255);not null"`
-	CreatedAt  time.Time `gorm:"type:timestamp;autoCreateTime"`
-	UpdatedAt  time.Time `gorm:"type:timestamp;autoUpdateTime"`
+	ID           uuid.UUID `gorm:"type:varchar(36);primaryKey"`
+	Partner      Partner
+	Ratings      []Rating
+	Transactions []Transaction
+	Name         string    `gorm:"type:varchar(255)"`
+	Username     string    `gorm:"type:varchar(255);unique;not null"`
+	Email        string    `gorm:"type:varchar(255);unique;not null"`
+	Phone        string    `gorm:"type:varchar(15);uniqueIndex;default:null"`
+	Address      string    `gorm:"type:text;default:null"`
+	BornDate     time.Time `gorm:"type:date;default:null"`
+	Gender       *Gender   `gorm:"type:ENUM('male','female');default:null"`
+	Password     string    `gorm:"type:varchar(255)"`
+	IsVerified   bool      `gorm:"type:boolean;default:false"`
+	PhotoURL     string    `gorm:"type:varchar(255);not null"`
+	CreatedAt    time.Time `gorm:"type:timestamp;autoCreateTime"`
+	UpdatedAt    time.Time `gorm:"type:timestamp;autoUpdateTime"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
