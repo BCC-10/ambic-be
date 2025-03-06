677c98c428c779c080d8244279474007d5957b04
atha3417 <atha.3417@gmail.com>
2025-03-06 13:38:18 +0700

    feat: get by partner id function

--- internal/app/product/repository/product.go
+++ internal/app/product/repository/product.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"ambic/internal/domain/dto"
 	"ambic/internal/domain/entity"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -11,6 +12,7 @@ type ProductMySQLItf interface {
 	Update(product *entity.Product) error
 	Show(product *entity.Product, param dto.ProductParam) error
 	Delete(product *entity.Product) error
+	GetByPartnerId(products *[]entity.Product, partnerId uuid.UUID, limit int, offset int) error
 }
 
 type ProductMySQL struct {
@@ -35,4 +37,12 @@ func (r *ProductMySQL) Update(product *entity.Product) error {
 
 func (r *ProductMySQL) Delete(product *entity.Product) error {
 	return r.db.Debug().Delete(product).Error
+}
+
+func (r *ProductMySQL) GetByPartnerId(product *[]entity.Product, partnerId uuid.UUID, limit int, offset int) error {
+	return r.db.Debug().Find(product, struct {
+		PartnerId uuid.UUID
+	}{
+		PartnerId: partnerId,
+	}).Error
 }
