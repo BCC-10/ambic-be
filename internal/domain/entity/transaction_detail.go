1854b04d680ecb6b4e8a9351fa8a311ed3cd024a
atha3417 <atha.3417@gmail.com>
2025-03-07 09:19:37 +0700

    fix: change Qty field type to uint in TransactionDetail struct

--- internal/domain/entity/transaction_detail.go
+++ internal/domain/entity/transaction_detail.go
@@ -9,7 +9,7 @@ type TransactionDetail struct {
 	ID            uuid.UUID `gorm:"type:char(36);primaryKey"`
 	TransactionID uuid.UUID `gorm:"type:char(36);not null"`
 	ProductID     uuid.UUID `gorm:"type:char(36);not null"`
-	Qty           int       `gorm:"type:int;not null"`
+	Qty           uint      `gorm:"type:int;not null"`
 }
 
 func (d *TransactionDetail) BeforeCreate(tx *gorm.DB) (err error) {
