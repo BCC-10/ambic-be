5e98a6e026702399ef39d9e279d0c2dc1f440084
atha3417 <atha.3417@gmail.com>
2025-03-07 19:50:03 +0700

    chore: update transaction status to include cancellation reasons

--- internal/domain/entity/transaction.go
+++ internal/domain/entity/transaction.go
@@ -9,10 +9,11 @@ import (
 type Status string
 
 const (
-	WaitingForPayment Status = "waiting for payment"
-	Finish            Status = "finish"
-	Process           Status = "process"
-	Cancelled         Status = "cancelled"
+	WaitingForPayment  Status = "waiting for payment"
+	Finish             Status = "finish"
+	Process            Status = "process"
+	CancelledByUser    Status = "cancelled by user"
+	CancelledByPartner Status = "cancelled by partner"
 )
 
 type Transaction struct {
@@ -22,7 +23,7 @@ type Transaction struct {
 	TransactionDetails []TransactionDetail
 	Invoice            string    `gorm:"type:varchar(255);not null;uniqueIndex"`
 	Total              float32   `gorm:"type:float(24);not null"`
-	Status             Status    `gorm:"type:ENUM('waiting for payment','finish','process','cancelled');default:null"`
+	Status             Status    `gorm:"type:ENUM('waiting for payment','finish','process','cancelled by user','cancelled by partner');default:null"`
 	Note               string    `gorm:"type:text"`
 	CreatedAt          time.Time `gorm:"type:timestamp;autoCreateTime"`
 	UpdatedAt          time.Time `gorm:"type:timestamp;autoUpdateTime"`
