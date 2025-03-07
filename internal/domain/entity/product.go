516b5bbddb0fba4816bfd1f5381e66e8c15c3757
atha3417 <atha.3417@gmail.com>
2025-03-07 09:18:33 +0700

    fix: change ID and PartnerID field types to char(36) in Product struct and update Stock type to uint

--- internal/domain/entity/product.go
+++ internal/domain/entity/product.go
@@ -8,19 +8,20 @@ import (
 )
 
 type Product struct {
-	ID           uuid.UUID `gorm:"type:varchar(36);primaryKey"`
-	PartnerID    uuid.UUID `gorm:"type:varchar(36);not null"`
-	Partner      Partner
-	Ratings      []Rating
-	Name         string    `gorm:"type:varchar(255);not null;uniqueIndex:idx_partner_product"`
-	Description  string    `gorm:"type:text;not null;"`
-	InitialPrice float32   `gorm:"type:float;not null"`
-	FinalPrice   float32   `gorm:"type:float;not null"`
-	Stock        int       `gorm:"type:int;not null"`
-	PickupTime   string    `gorm:"type:varchar(30);not null"`
-	PhotoURL     string    `gorm:"type:varchar(255)"`
-	CreatedAt    time.Time `gorm:"type:timestamp;autoCreateTime"`
-	UpdatedAt    time.Time `gorm:"type:timestamp;autoUpdateTime"`
+	ID                 uuid.UUID `gorm:"type:char(36);primaryKey"`
+	PartnerID          uuid.UUID `gorm:"type:char(36);not null"`
+	Partner            Partner
+	Ratings            []Rating
+	TransactionDetails []TransactionDetail
+	Name               string    `gorm:"type:varchar(255);not null;uniqueIndex:idx_partner_product"`
+	Description        string    `gorm:"type:text;not null;"`
+	InitialPrice       float32   `gorm:"type:float;not null"`
+	FinalPrice         float32   `gorm:"type:float;not null"`
+	Stock              uint      `gorm:"type:int;not null"`
+	PickupTime         string    `gorm:"type:varchar(30);not null"`
+	PhotoURL           string    `gorm:"type:varchar(255)"`
+	CreatedAt          time.Time `gorm:"type:timestamp;autoCreateTime"`
+	UpdatedAt          time.Time `gorm:"type:timestamp;autoUpdateTime"`
 }
 
 func (p *Product) BeforeCreate(tx *gorm.DB) (err error) {
