c2bffc1e9d28c8fbc06a6b06ad36c95334bc2e71
atha3417 <atha.3417@gmail.com>
2025-03-08 08:34:08 +0700

    chore: implement GeneratePaymentLink method in Midtrans for payment URL generation

--- internal/infra/midtrans/midtrans.go
+++ internal/infra/midtrans/midtrans.go
@@ -1,12 +1,14 @@
 package midtrans
 
 import (
+	"ambic/internal/domain/dto"
 	"ambic/internal/domain/env"
 	"github.com/midtrans/midtrans-go"
 	"github.com/midtrans/midtrans-go/snap"
 )
 
 type MidtransIf interface {
+	GeneratePaymentLink(req dto.RequestSnap) (string, error)
 }
 
 type Midtrans struct {
@@ -20,4 +22,44 @@ func New(env *env.Env) *Midtrans {
 	return &Midtrans{
 		Snap: s,
 	}
+}
+
+func (m *Midtrans) GeneratePaymentLink(req dto.RequestSnap) (string, error) {
+	custAddress := &midtrans.CustomerAddress{
+		FName:       req.User.Name,
+		Phone:       req.User.Phone,
+		Address:     req.User.Address,
+		CountryCode: "IDN",
+	}
+
+	snapReq := &snap.Request{
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  req.OrderID,
+			GrossAmt: req.Amount,
+		},
+		CustomerDetail: &midtrans.CustomerDetails{
+			FName:    req.User.Name,
+			Email:    req.User.Email,
+			Phone:    req.User.Phone,
+			BillAddr: custAddress,
+		},
+		CustomField1: req.TransactionID,
+	}
+
+	var items []midtrans.ItemDetails
+	for _, p := range req.TransactionDetails {
+		items = append(items, midtrans.ItemDetails{
+			ID:    p.ProductID,
+			Price: int64(p.Product.FinalPrice),
+			Qty:   int32(p.Qty),
+			Name:  p.Product.Name,
+		})
+	}
+
+	url, err := m.Snap.CreateTransactionUrl(snapReq)
+	if err != nil {
+		return "", err
+	}
+
+	return url, nil
 }
