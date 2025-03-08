2a976bdcb240fa8af2d1d73263611100c2fec657
atha3417 <atha.3417@gmail.com>
2025-03-08 08:33:23 +0700

    chore: implement payment link generation in TransactionUsecase

--- internal/domain/dto/payment.go
+++ internal/domain/dto/payment.go
@@ -1,25 +1,43 @@
 package dto
 
+import "github.com/google/uuid"
+
 type NotificationPayment struct {
-	OrderID           string `json:"order_id" validate:"required"`
-	ReferenceID       string `json:"reference_id" validate:"required"`
-	TransactionStatus string `json:"transaction_status" validate:"required"`
-	StatusMessage     string `json:"status_message" validate:"required"`
-	PaymentType       string `json:"payment_type" validate:"required"`
-	FraudStatus       string `json:"fraud_status" validate:"required"`
-	TransactionTime   string `json:"transaction_time" validate:"required"`
-	SettlementTime    string `json:"settlement_time" validate:"required"`
+	OrderID           string `json:"order_id"`
+	TransactionID     string `json:"custom_field1"`
+	ReferenceID       string `json:"reference_id"`
+	MerchantID        string `json:"merchant_id"`
+	Issuer            string `json:"issuer"`
+	GrossAmount       string `json:"gross_amount"`
+	Currency          string `json:"currency"`
+	Acquirer          string `json:"acquirer"`
+	TransactionStatus string `json:"transaction_status"`
+	StatusMessage     string `json:"status_message"`
+	PaymentType       string `json:"payment_type"`
+	FraudStatus       string `json:"fraud_status"`
+	TransactionTime   string `json:"transaction_time"`
+	SettlementTime    string `json:"settlement_time"`
 }
 
 type GetPaymentResponse struct {
-	ID                string `json:"id,omitempty"`
-	TransactionID     string `json:"transaction_id,omitempty"`
-	OrderID           string `json:"order_id,omitempty"`
-	ReferenceID       string `json:"reference_id,omitempty"`
-	TransactionStatus string `json:"transaction_status,omitempty"`
-	StatusMessage     string `json:"status_message,omitempty"`
-	PaymentType       string `json:"payment_type,omitempty"`
-	FraudStatus       string `json:"fraud_status,omitempty"`
-	TransactionTime   string `json:"transaction_time,omitempty"`
-	SettlementTime    string `json:"settlement_time,omitempty"`
+	ID                string  `json:"id,omitempty"`
+	OrderID           string  `json:"order_id,omitempty"`
+	TransactionID     string  `json:"custom_field1,omitempty"`
+	ReferenceID       string  `json:"reference_id,omitempty"`
+	MerchantID        string  `json:"merchant_id,omitempty"`
+	Issuer            string  `json:"issuer,omitempty"`
+	GrossAmount       float32 `json:"gross_amount,omitempty"`
+	Currency          string  `json:"currency,omitempty"`
+	Acquirer          string  `json:"acquirer,omitempty"`
+	TransactionStatus string  `json:"transaction_status,omitempty"`
+	StatusMessage     string  `json:"status_message,omitempty"`
+	PaymentType       string  `json:"payment_type,omitempty"`
+	FraudStatus       string  `json:"fraud_status,omitempty"`
+	TransactionTime   string  `json:"transaction_time,omitempty"`
+	SettlementTime    string  `json:"settlement_time,omitempty"`
+}
+
+type PaymentParam struct {
+	ID            uuid.UUID
+	TransactionID uuid.UUID
 }
