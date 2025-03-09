bb54a8956e334be566099e717332f19c9cdc1969
atha3417 <atha.3417@gmail.com>
2025-03-09 22:15:33 +0700

    chore: add Notifications field to User struct for enhanced user management

---
+++ internal/domain/entity/notification.go
@@ -0,0 +1,33 @@
+package entity
+
+import (
+	"ambic/internal/domain/dto"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Notification struct {
+	ID        uuid.UUID `gorm:"type:char(36);primaryKey"`
+	UserID    uuid.UUID `gorm:"type:char(36)"`
+	Title     string    `gorm:"type:varchar(255)"`
+	Content   string    `gorm:"type:varchar(255)"`
+	Link      string    `gorm:"type:varchar(255)"`
+	CreatedAt time.Time `gorm:"type:timestamp;autoCreateTime"`
+	UpdatedAt time.Time `gorm:"type:timestamp;autoUpdateTime"`
+}
+
+func (n *Notification) BeforeCreate(tx *gorm.DB) (err error) {
+	n.ID, _ = uuid.NewUUID()
+	return
+}
+
+func (n *Notification) ParseDTOGet() dto.GetNotificationResponse {
+	return dto.GetNotificationResponse{
+		ID:       n.ID.String(),
+		Title:    n.Title,
+		Content:  n.Content,
+		Link:     n.Link,
+		Datetime: n.CreatedAt.String(),
+	}
+}
