4938628cc19de3a67e3a8eadd4a24bf74b0b328a
atha3417 <atha.3417@gmail.com>
2025-03-13 16:00:27 +0700

    feat: add PartnerRegistrationTelegramMessage struct and partner registration notification template

--- internal/domain/dto/dto.go
+++ internal/domain/dto/dto.go
@@ -10,3 +10,23 @@ type Location struct {
 	Lat  float64
 	Long float64
 }
+
+type PartnerRegistrationTelegramMessage struct {
+	UserID               string
+	UserName             string
+	UserUsername         string
+	UserEmail            string
+	UserPhone            string
+	UserAddress          string
+	UserGender           string
+	UserRegisteredAt     string
+	PartnerID            string
+	BusinessType         string
+	BusinessName         string
+	BusinessAddress      string
+	BusinessCity         string
+	BusinessGmaps        string
+	BusinessInstagram    string
+	BusinessPhoto        string
+	BusinessRegisteredAt string
+}
